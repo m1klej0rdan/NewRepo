3f85d3ed525a58fed1f2891b31f758821996bfdc
m1klej0rdan <liuda2761@gmail.com>
2025-11-22 00:59:31 +0200

    Add files via upload

---
+++ lab 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x;
+    int i;
+
+    std::cout << "число: ";
+    std::cin >> x;
+    std::cout << "номер бита: ";
+    std::cin >> i;
+    int mask = 1;
+    if (i == 1) mask = 2;
+    int result = x + mask;
+
+    std::cout << "стало: " << result << std::endl;
+
+    return 0;
+}
