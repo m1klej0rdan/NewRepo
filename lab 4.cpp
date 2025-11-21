28bb0f014bd887b43900232a67814edc294acd7d
m1klej0rdan <liuda2761@gmail.com>
2025-11-22 01:01:19 +0200

    Add files via upload

---
+++ lab 4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+        if (N % i == 0) {
+            std::cout << i << " ";
+            count = count + 1;
+            if (count >= 1000) {
+                std::cout << "Too many divisors";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
